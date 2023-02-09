9472bc5cfcad694286e0583399c148766e8593e1
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2023-02-09 15:13:16 -0300

    feat: starting Vue project with imports (Bootstrap, Font Awesome and Google Fonts)

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
+</script>
+
+
+
+<template>
+  <Header title="Pokedex Premium"></Header>
+  <Footer></Footer>
+  <!-- <header>
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView /> -->
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="col-4 mx-auto">
+    <p class="text-center m-0">Project by Gabriel Soares</p>
+    <div class="d-flex justify-content-center gap-2">
+      <a href="https://www.linkedin.com/in/gabrielsoarespebr/" target="_blank">
+        <i class="text-white fa-brands fa-linkedin"></i>
+      </a>
+
+      <a href="https://github.com/gabrielsoarespebr" target="_blank">
+        <i class="text-white fa-brands fa-square-github"></i>
+      </a>
+
+    </div>
+
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
